3ec78d2740696c455d463f99f922d8adba9c6fbb
Parth Solanki <parth@instnt.org>
2021-12-03 20:23:19 +0530

    IN-1688, Changes related to working opening camera

--- Instnt/instntsdk/src/main/java/org/instant/accept/instntsdk/authentication/AuthenticID.java
+++ Instnt/instntsdk/src/main/java/org/instant/accept/instntsdk/authentication/AuthenticID.java
@@ -6,6 +6,7 @@ import com.idmetrics.dc.DSHandler;
 import com.idmetrics.dc.utils.DSCaptureMode;
 import com.idmetrics.dc.utils.DSError;
 import com.idmetrics.dc.utils.DSHandlerListener;
+import com.idmetrics.dc.utils.DSID1Options;
 import com.idmetrics.dc.utils.DSOptions;
 import com.idmetrics.dc.utils.DSResult;
 
@@ -16,12 +17,12 @@ public class AuthenticID {
     }
 
     public void processToCallScan(android.content.Context context) {
-        DSOptions dsOptions = new DSOptions();
+        DSID1Options dsOptions = new DSID1Options();
         dsOptions.licensingKey = "AwFuEf5j3YXwEACwj9eE4w6RGWQ0zgPbjGmu+Xw684ryGP3GicSEE7ZYB0FAhoikRH3imeR02U7kuT4OjVL5B1s3JhBrPY9KWU9sgCVmTIW0r7ehq9CvTjTBfaR7NTCV179MlNeDbEzwh5FSD8ROc3Zq";
         scanIdDocument(dsOptions, true, context);
     }
 
-    private void scanIdDocument(DSOptions settings, Boolean isFront, android.content.Context context) {
+    private void scanIdDocument(DSID1Options settings, Boolean isFront, android.content.Context context) {
 
         DSHandler dsHandler = DSHandler.getInstance(context);
         DSHandler.staticLicenseKey = "AwFuEf5j3YXwEACwj9eE4w6RGWQ0zgPbjGmu+Xw684ryGP3GicSEE7ZYB0FAhoikRH3imeR02U7kuT4OjVL5B1s3JhBrPY9KWU9sgCVmTIW0r7ehq9CvTjTBfaR7NTCV179MlNeDbEzwh5FSD8ROc3Zq";
