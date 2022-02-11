80e325d15010640972cc742d3b7bee4bc61ed094
Parth Solanki <parth@instnt.org>
2022-02-11 13:45:29 +0000

    Squashed commit of the following
    commit 9fa7cbfb 
    Author: Parth Solanki <parth@instnt.org> 
    Date: Fri Feb 11 2022 19:14:32 GMT+0530 (India Standard Time) 
    
        IN-1881, Changes related to comment on instntSDK all public methods

--- Instnt/instntsdk/src/main/java/org/instnt/accept/instntsdk/InstntSDK.java
+++ Instnt/instntsdk/src/main/java/org/instnt/accept/instntsdk/InstntSDK.java
@@ -16,22 +16,84 @@ public interface InstntSDK {
 
     InstntSDK instance = new InstntSDKImpl();
 
+    /**
+     * Initialize instntSDK
+     * @param formKey
+     * @param serverUrl
+     * @param callbackHandler
+     * @return
+     */
     static InstntSDK init(String formKey, String serverUrl, CallbackHandler callbackHandler) {
         instance.initTransaction(formKey, serverUrl, callbackHandler);
         return instance;
     }
 
+    /**
+     * Initialize transaction
+     * @param formKey
+     * @param serverUrl
+     * @param callbackHandler
+     */
     void initTransaction(String formKey, String serverUrl, CallbackHandler callbackHandler);
+
+    /**
+     * Get instnt transaction id
+     * @return
+     */
     String getInstnttxnid();
+
+    /**
+     * Check is otp verification enabled
+     * @return
+     */
     boolean isOTPverificationEnabled();
+
+    /**
+     * Check is document verification enabled
+     * @return
+     */
     boolean isDocumentVerificationEnabled();
 
+    /**
+     * Scan document
+     * @param isFront
+     * @param isAutoUpload
+     * @param documentType
+     * @param context
+     * @param documentVerifyLicenseKey
+     */
     void scanDocument(boolean ifFront, boolean isAutoUpload, String documentType, Context context, String documentVerifyLicenseKey);
+
+    /**
+     * Upload attachment
+     * @param ifFront
+     */
     void uploadAttachment(boolean ifFront);
+
+    /**
+     * Verify documents
+     * @param documentType
+     */
     void verifyDocuments(String documentType);
 
+    /**
+     * Submit form
+     * @param context
+     * @param windowManager
+     * @param body
+     */
     void submitData(Context context, WindowManager windowManager, Map<String, Object> body);
 
+    /**
+     * Send OTP
+     * @param mobileNumber
+     */
     void sendOTP(String mobileNumber);
+
+    /**
+     * Verify OTP
+     * @param mobileNumber
+     * @param otpCode
+     */
     void verifyOTP(String mobileNumber, String otpCode);
 }
